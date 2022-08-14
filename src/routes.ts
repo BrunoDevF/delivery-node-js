c750266e3be378321cdf84eb12ced290ad3198b3
BrunoDevF <brunohenriquex00@gmail.com>
2022-08-14 10:15:29 -0300

    feat: rota de autenticacao de entregador

--- src/routes.ts
+++ src/routes.ts
@@ -4,8 +4,10 @@ const routes = Router();
 import { createClientController } from './modules/clients/useCases/createClient';
 import { authenticateClientController } from './modules/account/authenticateClient'
 import { createDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman'
+import { authenticateDeliverymanController } from './modules/account/authenticateDeliveryman'
 
-routes.post('/authenticate', authenticateClientController.handle)
+routes.post('/client/authenticate', authenticateClientController.handle)
+routes.post('/deliveryman/authenticate', authenticateDeliverymanController.handle)
 
 routes.post('/clients', createClientController.handle)
 
