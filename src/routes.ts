7a6db0517a25e5bbc2efaa51899cc5561ee44ff7
BrunoDevF <brunohenriquex00@gmail.com>
2022-08-14 10:01:39 -0300

    feat: rota para adicionar um novo entregador

--- src/routes.ts
+++ src/routes.ts
@@ -3,9 +3,12 @@ const routes = Router();
 
 import { createClientController } from './modules/clients/useCases/createClient';
 import { authenticateClientController } from './modules/account/authenticateClient'
+import { createDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman'
 
 routes.post('/authenticate', authenticateClientController.handle)
 
 routes.post('/clients', createClientController.handle)
+
+routes.post('/deliveryman', createDeliverymanController.handle)
 
 export default routes
