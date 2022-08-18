ca160e99c0d365f0f69b5af21816de4a88b42d36
BrunoDevF <brunohenriquex00@gmail.com>
2022-08-18 18:24:55 -0300

    feat: criado rota para cadastrar uma entrega

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import { createClientController } from './modules/clients/useCases/createClient'
 import { authenticateClientController } from './modules/account/authenticateClient'
 import { createDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman'
 import { authenticateDeliverymanController } from './modules/account/authenticateDeliveryman'
+import { createDeliveryController } from './modules/deliveries/useCases/createDelivery'
 
 routes.post('/client/authenticate', authenticateClientController.handle)
 routes.post('/deliveryman/authenticate', authenticateDeliverymanController.handle)
@@ -12,5 +13,6 @@ routes.post('/deliveryman/authenticate', authenticateDeliverymanController.handl
 routes.post('/clients', createClientController.handle)
 
 routes.post('/deliveryman', createDeliverymanController.handle)
+routes.post('/delivery', createDeliveryController.handle)
 
 export default routes
