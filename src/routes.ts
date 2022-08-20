67b1785b16578e0e768b28d15bbb3b568c9d3504
bruno_henr <brunohenriquex0@gmail.com>
2022-08-20 09:52:49 -0300

    feat: rotas de entregas disponiveis e de atualizar entrega

--- src/routes.ts
+++ src/routes.ts
@@ -1,18 +1,42 @@
 import { Router } from "express";
 const routes = Router();
 
-import { createClientController } from './modules/clients/useCases/createClient';
-import { authenticateClientController } from './modules/account/authenticateClient'
-import { createDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman'
-import { authenticateDeliverymanController } from './modules/account/authenticateDeliveryman'
-import { createDeliveryController } from './modules/deliveries/useCases/createDelivery'
+import { createClientController } from "./modules/clients/useCases/createClient";
+import { authenticateClientController } from "./modules/account/authenticateClient";
+import { createDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman";
+import { authenticateDeliverymanController } from "./modules/account/authenticateDeliveryman";
+import { createDeliveryController } from "./modules/deliveries/useCases/createDelivery";
+import { findAllAvailableController } from "./modules/deliveries/useCases/findAllAvailable";
+import { updateDeliverymanController } from "./modules/deliveries/useCases/updateDeliveryman/useCases";
+// middlewares
+import { ensureAuthenticateClient } from "./middlewares/ensureAuthenticateClient";
+import { ensureAuthenticateDeliveryman } from "./middlewares/ensureAuthenticateDeliveryman";
 
-routes.post('/client/authenticate', authenticateClientController.handle)
-routes.post('/deliveryman/authenticate', authenticateDeliverymanController.handle)
+routes.post("/client/authenticate", authenticateClientController.handle);
+routes.post(
+  "/deliveryman/authenticate",
+  authenticateDeliverymanController.handle
+);
 
-routes.post('/clients', createClientController.handle)
+routes.post("/clients", createClientController.handle);
 
-routes.post('/deliveryman', createDeliverymanController.handle)
-routes.post('/delivery', createDeliveryController.handle)
+routes.post("/deliveryman", createDeliverymanController.handle);
+routes.post(
+  "/delivery",
+  ensureAuthenticateClient,
+  createDeliveryController.handle
+);
 
-export default routes
+routes.get(
+  "/delivery/available",
+  ensureAuthenticateDeliveryman,
+  findAllAvailableController.handle
+);
+
+routes.put(
+    "/delivery/updateDeliveryman",
+    ensureAuthenticateDeliveryman,
+    updateDeliverymanController.handle
+  );
+
+export default routes;
