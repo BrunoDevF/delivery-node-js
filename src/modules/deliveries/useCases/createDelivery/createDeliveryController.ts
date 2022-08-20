b49f3df534fa346db7b9c8f20987c12a92ab8044
bruno_henr <brunohenriquex0@gmail.com>
2022-08-20 09:51:44 -0300

    refac: recuperando id do client pelo request passado pelo middleware

--- src/modules/deliveries/useCases/createDelivery/createDeliveryController.ts
+++ src/modules/deliveries/useCases/createDelivery/createDeliveryController.ts
@@ -3,12 +3,13 @@ import { CreateDeliveryUseCase } from './createDeliveryUseCase';
 
 export class CreateDeliveryController {
     async handle(request: Request, response: Response) {
-        const { id_client, item_name } = request.body
+        const { item_name } = request.body;
+        const { id_client } = request;
         const createDelivery = new CreateDeliveryUseCase();
-        const result = await createDelivery.execute({ 
-            id_client, item_name 
+        const result = await createDelivery.execute({
+            id_client, item_name
         });
-        if(!result.has_error) {
+        if (!result.has_error) {
             return response.status(200).json(result);
         }
         return response.status(400).json(result);
